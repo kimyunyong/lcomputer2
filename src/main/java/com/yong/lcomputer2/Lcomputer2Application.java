edef00cb8751fc35a286ee9aa4f21cd351e9e5a9
kyy3326 <kyy3326@gmail.com>
2021-02-14 22:07:35 +0900

    first commit

---
+++ src/main/java/com/yong/lcomputer2/Lcomputer2Application.java
@@ -0,0 +1,13 @@
+package com.yong.lcomputer2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lcomputer2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lcomputer2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/yong/lcomputer2/Lcomputer2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yong.lcomputer2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lcomputer2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
